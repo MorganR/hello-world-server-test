1ffd084af24ed55522308acb68d79cce7cd94d9b
Morgan Roff <roffmorgan@gmail.com>
2023-03-21 17:04:20 +0100

    Add "/strings/lines" test

--- integration_test.go
+++ integration_test.go
@@ -6,12 +6,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/valyala/fasthttp"
 )
+
+const EXPECTED_SLOW_TIME = time.Millisecond * 15
 
 var (
 	baseUrl = flag.String("base_url", "", "The base URL (scheme + host + port), to run integration tests against. Example: http://localhost:80")
@@ -40,7 +44,7 @@ func TestMain(m *testing.M) {
 
 func TestHello(t *testing.T) {
 	uri := getBaseUri()
-	uri.SetPath("/hello")
+	uri.SetPath("/strings/hello")
 
 	req := fasthttp.AcquireRequest()
 	req.SetURI(uri)
@@ -49,12 +53,12 @@ func TestHello(t *testing.T) {
 	if err != nil {
 		t.Fatalf("request failed: %v", err.Error())
 	}
-	verifyUncompressedHelloResponse(resp, "Hello, world!", t)
+	verifyUncompressedTextResponse(resp, "Hello, world!", t)
 }
 
 func TestHelloWithName(t *testing.T) {
 	uri := getBaseUri()
-	uri.SetPath("/hello")
+	uri.SetPath("/strings/hello")
 	args := uri.QueryArgs()
 	args.Add("name", `some COOL guy`)
 
@@ -65,12 +69,12 @@ func TestHelloWithName(t *testing.T) {
 	if err != nil {
 		t.Fatalf("request failed: %v", err.Error())
 	}
-	verifyUncompressedHelloResponse(resp, "Hello, some COOL guy!", t)
+	verifyUncompressedTextResponse(resp, "Hello, some COOL guy!", t)
 }
 
 func TestHelloWithEmptyName(t *testing.T) {
 	uri := getBaseUri()
-	uri.SetPath("/hello")
+	uri.SetPath("/strings/hello")
 	args := uri.QueryArgs()
 	args.Add("name", "")
 
@@ -81,13 +85,13 @@ func TestHelloWithEmptyName(t *testing.T) {
 	if err != nil {
 		t.Fatalf("request failed: %v", err.Error())
 	}
-	verifyUncompressedHelloResponse(resp, "Hello, world!", t)
+	verifyUncompressedTextResponse(resp, "Hello, world!", t)
 }
 
 func TestHelloNameMaxLength(t *testing.T) {
 	// Max length should succeed.
 	uri := getBaseUri()
-	uri.SetPath("/hello")
+	uri.SetPath("/strings/hello")
 	args := uri.QueryArgs()
 	maxLenName := strings.Repeat("a", 500)
 	args.Set("name", maxLenName)
@@ -99,7 +103,7 @@ func TestHelloNameMaxLength(t *testing.T) {
 	if err != nil {
 		t.Fatalf("max len name request failed: %v", err.Error())
 	}
-	verifyUncompressedHelloResponse(resp, fmt.Sprintf("Hello, %v!", maxLenName), t)
+	verifyUncompressedTextResponse(resp, fmt.Sprintf("Hello, %v!", maxLenName), t)
 
 	// Too long should fail.
 	args.Set("name", maxLenName+"a")
@@ -120,7 +124,7 @@ func TestHelloNameMaxLength(t *testing.T) {
 
 func TestHelloCompression(t *testing.T) {
 	uri := getBaseUri()
-	uri.SetPath("/hello")
+	uri.SetPath("/strings/hello")
 	args := uri.QueryArgs()
 	// Use max length since some frameworks don't compress small responses.
 	maxLenName := strings.Repeat("a", 500)
@@ -134,21 +138,73 @@ func TestHelloCompression(t *testing.T) {
 	if err != nil {
 		t.Fatalf("request failed: %v", err.Error())
 	}
-	verifyHelloTypeAndCode(resp, t)
-	gotEncoding := string(resp.Header.ContentEncoding())
-	wantEncoding := "br"
-	if gotEncoding != wantEncoding {
-		t.Fatalf("invalid encoding. Want: %v, got: %v", wantEncoding, gotEncoding)
-	}
-	uncompressed, err := resp.BodyUnbrotli()
-	if err != nil {
-		t.Fatalf("failed to uncompress: %v", err.Error())
-	}
-	gotBody := string(uncompressed)
 	wantBody := "Hello, " + maxLenName + "!"
-	if gotBody != wantBody {
-		t.Errorf("invalid body. Want: %v, got: %v", wantBody, gotBody)
+	verifyCompressedTextResponse(resp, wantBody, t)
+}
+
+func TestAsyncHello(t *testing.T) {
+	uri := getBaseUri()
+	uri.SetPath("/strings/async-hello")
+
+	req := fasthttp.AcquireRequest()
+	req.SetURI(uri)
+	start := time.Now()
+	resp, err := doRequest(req)
+	end := time.Now()
+
+	if err != nil {
+		t.Fatalf("request failed: %v", err.Error())
 	}
+	req_time := end.Sub(start)
+	if req_time < EXPECTED_SLOW_TIME {
+		t.Errorf("request took %v, expected at least %v", req_time, EXPECTED_SLOW_TIME)
+	}
+	verifyUncompressedTextResponse(resp, "Hello, world!", t)
+}
+
+func TestLines(t *testing.T) {
+	uri := getBaseUri()
+	uri.SetPath("/strings/lines")
+	uri.QueryArgs().Add("n", "4")
+
+	req := fasthttp.AcquireRequest()
+	req.SetURI(uri)
+	resp, err := doRequest(req)
+
+	if err != nil {
+		t.Fatalf("request failed: %v", err.Error())
+	}
+	verifyUncompressedTextResponse(resp,
+		`<ol>
+  <li>Item number: 1</li>
+  <li>Item number: 2</li>
+  <li>Item number: 3</li>
+  <li>Item number: 4</li>
+</ol>`,
+		t)
+}
+
+func TestLinesLongResponseIsCompressed(t *testing.T) {
+	uri := getBaseUri()
+	uri.SetPath("/strings/lines")
+	n := 100
+	uri.QueryArgs().Add("n", strconv.Itoa(n))
+
+	req := fasthttp.AcquireRequest()
+	req.SetURI(uri)
+	req.Header.Set(fasthttp.HeaderAcceptEncoding, "unknown, br")
+	resp, err := doRequest(req)
+
+	if err != nil {
+		t.Fatalf("request failed: %v", err.Error())
+	}
+	wantBody := strings.Builder{}
+	wantBody.WriteString("<ol>\n")
+	for i := 1; i <= n; i++ {
+		wantBody.WriteString(fmt.Sprintf("<li>Item number: %v</li>\n", i))
+	}
+	wantBody.WriteString("</ol>")
+	verifyCompressedTextResponse(resp, wantBody.String(), t)
 }
 
 var expectedInvalidPaths = []string{
@@ -249,6 +305,63 @@ func TestStaticImage(t *testing.T) {
 	}
 }
 
+var expectedPowerReciprocalAltResults = map[int]float64{
+	0:   0,
+	1:   1,
+	100: 0.666,
+}
+
+func TestMathPowerReciprocalsAlt(t *testing.T) {
+	for n, want := range expectedPowerReciprocalAltResults {
+		uri := getBaseUri()
+		uri.SetPath("/math/power-reciprocals-alt")
+		uri.QueryArgs().Add("n", strconv.Itoa(n))
+
+		req := fasthttp.AcquireRequest()
+		req.SetURI(uri)
+		resp, err := doRequest(req)
+
+		if err != nil {
+			t.Fatalf("request failed: %v", err.Error())
+		}
+		verifyTextTypeAndCode(resp, t)
+		gotBody := string(resp.Body())
+		gotNum, err := strconv.ParseFloat(gotBody, 64)
+		if err != nil {
+			t.Errorf("could not parse response to float: %v", gotBody)
+		}
+		wantLow, wantHigh := want-0.001, want+0.001
+		if gotNum < wantLow || gotNum > wantHigh {
+			t.Errorf("Got %.3f, want a number between %.3f and %.3f", gotNum, wantLow, wantHigh)
+		}
+	}
+}
+
+func TestMathPowerReciprocalsAltIsNotCompressed(t *testing.T) {
+	uri := getBaseUri()
+	uri.SetPath("/math/power-reciprocals-alt")
+	uri.QueryArgs().Add("n", "100")
+
+	req := fasthttp.AcquireRequest()
+	req.SetURI(uri)
+	req.Header.Add(fasthttp.HeaderAcceptEncoding, "br, gzip")
+	resp, err := doRequest(req)
+
+	if err != nil {
+		t.Fatalf("request failed: %v", err.Error())
+	}
+	verifyTextTypeAndCode(resp, t)
+	gotBody := string(resp.Body())
+	gotNum, err := strconv.ParseFloat(gotBody, 64)
+	if err != nil {
+		t.Errorf("could not parse response to float: %v", gotBody)
+	}
+	wantLow, wantHigh := 0.1, 1.0
+	if gotNum < wantLow || gotNum > wantHigh {
+		t.Errorf("Got %.3f, want a number between %.3f and %.3f", gotNum, wantLow, wantHigh)
+	}
+}
+
 func getBaseUri() *fasthttp.URI {
 	uri := fasthttp.AcquireURI()
 	baseUri.CopyTo(uri)
@@ -261,15 +374,32 @@ func doRequest(r *fasthttp.Request) (*fasthttp.Response, error) {
 	return resp, err
 }
 
-func verifyUncompressedHelloResponse(got *fasthttp.Response, wantBody string, t *testing.T) {
-	verifyHelloTypeAndCode(got, t)
+func verifyUncompressedTextResponse(got *fasthttp.Response, wantBody string, t *testing.T) {
+	verifyTextTypeAndCode(got, t)
 	gotBody := string(got.Body())
 	if gotBody != wantBody {
 		t.Errorf("invalid body. Want: %v, got: %v", wantBody, gotBody)
 	}
 }
 
-func verifyHelloTypeAndCode(got *fasthttp.Response, t *testing.T) {
+func verifyCompressedTextResponse(got *fasthttp.Response, wantBody string, t *testing.T) {
+	verifyTextTypeAndCode(got, t)
+	gotEncoding := string(got.Header.ContentEncoding())
+	wantEncoding := "br"
+	if gotEncoding != wantEncoding {
+		t.Fatalf("invalid encoding; want: %v, got: %v", wantEncoding, gotEncoding)
+	}
+	uncompressed, err := got.BodyUnbrotli()
+	if err != nil {
+		t.Fatalf("failed to uncompress: %v", err.Error())
+	}
+	gotBody := string(uncompressed)
+	if gotBody != wantBody {
+		t.Errorf("invalid body; want: %v, got: %v", wantBody, gotBody)
+	}
+}
+
+func verifyTextTypeAndCode(got *fasthttp.Response, t *testing.T) {
 	verifyTypeAndCode(got, "text/plain; charset=utf-8", http.StatusOK, t)
 }
 
@@ -277,10 +407,10 @@ func verifyTypeAndCode(got *fasthttp.Response, wantType string, wantCode int, t 
 	gotType := string(got.Header.ContentType())
 
 	if !strings.HasPrefix(gotType, wantType) {
-		t.Errorf("invalid content type. Want prefix: %v, got: %v", wantType, gotType)
+		t.Errorf("invalid content type; want prefix: %v, got: %v", wantType, gotType)
 	}
 	gotCode := got.StatusCode()
 	if gotCode != wantCode {
-		t.Errorf("invalid status code. Want: %v, got: %v", wantCode, gotCode)
+		t.Errorf("invalid status code; want: %v, got: %v", wantCode, gotCode)
 	}
 }
